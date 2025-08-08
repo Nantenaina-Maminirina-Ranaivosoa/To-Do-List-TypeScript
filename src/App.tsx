41135203421d32b9104903a9b9fd5adde8c759e0
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-08-08 11:50:40 +0300

    feat: Définition du type Task et initialisation de l'état des tâches

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
 
+export interface Task {
+    id: number;
+    text : string;
+    completed: boolean;
+  }
+
 function App() {
+  const [tasks, setTasks] = useState<Task[]>([
+  {id :1, text: 'Apprendre TypeScript', completed: true},
+  {id :2, text: 'Créer une To-Do List', completed: false},
+  {id :3, text: 'Publier sur Github', completed: false},
+]);
+
   return(
     <div className='app-container'>
       <h1>Ma To-Do List</h1>
